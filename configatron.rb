9642dff5521c1db68e600da6917b0a87deb08d38
Eric Barnes <SyxUT@Gmail.com>
2015-09-07 00:35:20 -0600

    Removed configuration informaion in preperation for push

--- configatron.rb
+++ configatron.rb
@@ -3,9 +3,9 @@ require 'configatron'
 configatron.configure_from_hash(
 	lending_club:
 	{
-		authorization: 'XjVzPbmT55/TX7k6t5Uzwv2yT2M=',
-		account: 2628791,
-		portfolio_id: 59612258,  	# "Autoinvestor"
+		authorization: '',
+		account: '',
+		portfolio_id: '',
 		investment_amount: 25, 		# amount to invest per loan ($25 minimum)
 
 		api_version: 'v1',
@@ -14,7 +14,7 @@ configatron.configure_from_hash(
 	},
 	push_bullet:
 	{
-		api_key: 'M1MzGV2ynrIBCGWKvd9ReeAuJVK765Gc',
-		device_id: 'ujweNd5igMesjAiVsKnSTs' #iphone 5s
+		api_key: '',
+		device_id: ''
 	}
 )
